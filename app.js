a736f1ee0297b33a94c656d50c0cbc29e6fd9249
Aslam Doctor <aslam.doctor@gmail.com>
2018-01-16 11:44:40 +0530

    successfully working script with foldername passed as argument

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
 const fs = require('fs');
+const yargs = require('yargs');
 const hostile = require('hostile');
 
-var foldername = 'test';
-var hostname = foldername + '.local';
+var foldername = process.argv[2];
 
-var vhosts_path = `c:\\xampp\\apache\\conf\\extra\\httpd-vhosts.conf`;
-var vhosts_code = `\n\n<VirtualHost *:80>
-    DocumentRoot "C:/xampp/htdocs/${foldername}"
-    ServerName myproject.dev
-    <Directory "C:/xampp/htdocs/${foldername}">
+if(foldername !== undefined){
+	var hostname = foldername + '.local';
+
+	var vhosts_path = `c:\\xampp\\apache\\conf\\extra\\httpd-vhosts.conf`;
+	var vhosts_code = `\n\n<VirtualHost *:80>
+	DocumentRoot "C:/xampp/htdocs/${foldername}"
+	ServerName myproject.dev
+	<Directory "C:/xampp/htdocs/${foldername}">
 	Options Indexes FollowSymLinks ExecCGI Includes
-        Order allow,deny
-        Allow from all
-    </Directory>
+		Order allow,deny
+		Allow from all
+	</Directory>
 </VirtualHost>`;
 
+	// Add Host to Apache VHosts
+	fs.appendFile(vhosts_path, vhosts_code, function (err) {
+	  if (err) throw err;
+	  console.log('Apache Hostname added successfully. \nHostname: ' + hostname + ' \nPlease restart your Apache server.');
+	});
 
-// Add Host to Apache VHosts
-fs.appendFile(vhosts_path, vhosts_code, function (err) {
-  if (err) throw err;
-  console.log('Apache VHost added successfully.');
-});
 
-
-// Add Host to System
-hostile.set('127.0.0.1', hostname, function (err) {
-  if (err) {
-    console.error(err)
-  } else {
-    console.log('Host added successfully.')
-  }
-});
+	// Add Host to System
+	hostile.set('127.0.0.1', hostname, function (err) {
+	  if (err) {
+		console.error(err)
+	  } else {
+		console.log('Host added successfully.')
+	  }
+	});
+}
+else{
+	console.log('Please provide your folder name as argument');
+}
